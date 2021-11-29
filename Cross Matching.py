59a41b4f0c4ca629fed46f905b2c73d7e88cd4a0
ReiCohen <93777274+ReiCohen@users.noreply.github.com>
2021-11-29 23:05:33 +0200

    change

--- Cross Matching.py
+++ Cross Matching.py
@@ -12,15 +12,21 @@ def sphere_distance_fast(RA_1, Dec_1, RA_2, Dec_2):
 CLU = np.radians(pd.read_csv('CLU.csv').loc[:, ['RA', 'DE']].to_numpy())
 trex = np.radians(pd.read_csv('trex.csv').loc[:, ['ra', 'dec']].to_numpy())
 
-tolerance = (np.linspace(0.0675, 0.07, 22))
+tolerance = (np.linspace(0.0675, 0.07, 10))
 result_vec = []
 
 for tol in tqdm(tolerance):
+    Matches_Text_Tol = open("Matches list for tol =  " + str(tol) + '.txt', "w")
     count_match = 0
     for x in range((len(CLU))):
         distance_vec = sphere_distance_fast(CLU[x][0], CLU[x][1], trex[:, 0], trex[:, 1])
         count_match = count_match + len(distance_vec[distance_vec < tol])
+        if len(distance_vec[distance_vec < tol]) > 0:
+            Matches_Text_Tol.write(
+                "\n" + 'match between clu @ line ' + str(x) + ' RA is ' + str(np.degrees(CLU[x][0])) + ' DEC is ' + str(
+                    np.degrees(CLU[x][1])) + ' and trex @line ' + str(np.asarray(np.where(distance_vec < tol))) + "\n")
     result_vec.append(count_match)
+    Matches_Text_Tol.close
 
 print(result_vec)
 
