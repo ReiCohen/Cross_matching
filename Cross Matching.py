9fcc2daa5de963eb332064fdc7d85c4be881008d
ReiCohen <93777274+ReiCohen@users.noreply.github.com>
2021-11-29 21:06:39 +0200

    use sphere_distance_fast as ny.array

--- Cross Matching.py
+++ Cross Matching.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 def sphere_distance_fast(RA_1, Dec_1, RA_2, Dec_2):
@@ -11,19 +12,15 @@ def sphere_distance_fast(RA_1, Dec_1, RA_2, Dec_2):
 CLU = np.radians(pd.read_csv('CLU.csv').loc[:, ['RA', 'DE']].to_numpy())
 trex = np.radians(pd.read_csv('trex.csv').loc[:, ['ra', 'dec']].to_numpy())
 
-tolerance = (np.linspace(0.05, 0.1, 20))
-result_vec = np.zeros(len(tolerance))
+tolerance = (np.linspace(0.0675, 0.07, 20))
+result_vec = []
 
-count_int = 0
-for tol in tolerance:
+for tol in tqdm(tolerance):
     count_match = 0
     for x in range((len(CLU))):
-        print('run number ' + str(count_int) + ' @ ' + str(x / len(CLU) * 100)[0:3] + ' %')
-        for y in range((len(trex))):
-            if sphere_distance_fast(CLU[x][0], CLU[x][1], trex[y][0], trex[y][1]) <= tol:
-                count_match = count_match + 1
-    result_vec[count_int] = count_match
-    count_int = count_int + 1
+        distance_vec = sphere_distance_fast(CLU[x][0], CLU[x][1], trex[:, 0], trex[:, 1])
+        count_match = count_match + len(distance_vec[distance_vec < tol])
+    result_vec.append(count_match)
 
 print(result_vec)
 
@@ -31,4 +28,5 @@ plt.plot(tolerance, result_vec, 'ro')
 plt.xlabel('tolerance @ rad')
 plt.ylabel('number of cross-match')
 plt.savefig('fig.png')
+plt.grid()
 plt.show()
