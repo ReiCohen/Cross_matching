5885d4fb4f4189b9b9d1a0276a77008435ae2f69
ReiCohen <93777274+ReiCohen@users.noreply.github.com>
2021-12-25 14:49:18 +0200

    change 25.12.21

---
+++ Clu&Chandra.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import csv
+from sklearn.cluster import KMeans
+import math
+
+num_of_clusters_in_clu = 7
+
+plt.subplot(111, projection="aitoff")
+CLU = np.radians(pd.read_csv(os.path.join(os.getcwd(), 'data\\CLU.csv')).loc[:, ['RA', 'DE']].to_numpy())
+
+# use kmeans
+km = KMeans(n_clusters=num_of_clusters_in_clu, init='random', n_init=10, max_iter=300, tol=1e-04, random_state=0)
+y_km = km.fit_predict(CLU)
+
+# print the clusters
+for i in range(num_of_clusters_in_clu):
+    plt.scatter(CLU[y_km == i, 0] - 3.14, CLU[y_km == i, 1], label=str('cluster ' + str(i)))
+plt.legend(scatterpoints=1)
+plt.grid()
+
+# print the centers
+plt.scatter(
+    km.cluster_centers_[:, 0] - math.pi, km.cluster_centers_[:, 1],
+    s=250, marker='*',
+    c='red', edgecolor='black',
+    label='centroids'
+)
+print(np.degrees(km.cluster_centers_))
+plt.savefig('kmeans.png', dpi=300)
+
+# save the csv file to upload to chandra
+header = ['ra', 'dec']
+with open('Upload.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(np.degrees(km.cluster_centers_))
